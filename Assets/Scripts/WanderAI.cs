279a18e24ac1a5955aa88ef8fdd63dfa598086d5
rascar <to.constant@gmail.com>
2022-03-05 11:34:26 +0100

    Enhance wandering

--- Assets/Scripts/WanderAI.cs
+++ Assets/Scripts/WanderAI.cs
@@ -6,19 +6,38 @@ using UnityEngine.EventSystems;
 public class WanderAI : MonoBehaviour
 {
     [SerializeField]
-    float speed;
+    float minSpeed;
     [SerializeField]
-    float range;
+    float maxSpeed;
+    [SerializeField]
+    float minDistance;
     [SerializeField]
     float maxDistance;
+    [SerializeField]
+    float minWaitingTime;
+    [SerializeField]
+    float maxWaitingTime;
+
+    float minDelta;
+    Vector4 boardBounds;
+
+    float currentSpeed;
 
     float waitTime;
 
     Vector2 wayPoint;
 
+
+
+    public void Init(Vector3 boardBounds)
+    {
+        this.boardBounds = boardBounds;
+    }
+
+
+
     private void Start()
     {
-        //StartCoroutine(Wandering());
         SetNewDestination();
         waitTime = 1f;
     }
@@ -26,11 +45,11 @@ public class WanderAI : MonoBehaviour
     private void Update()
     {
         waitTime += Time.deltaTime;
-        Debug.Log("Wait Time = " + waitTime);
+        // Debug.Log("Wait Time = " + waitTime);
 
         if (waitTime > 8)
         {
-            
+
             waitTime = 0f;
         }
 
@@ -41,31 +60,41 @@ public class WanderAI : MonoBehaviour
         else
         {
             Move();
-
         }
 
-        if (Vector2.Distance(transform.position, wayPoint) <range)
+        if (Vector2.Distance(transform.position, wayPoint) < minDelta)
         {
-
             SetNewDestination();
-            //StartCoroutine(Wandering()) ;
-            
         }
     }
 
-    void SetNewDestination ()
+    private void SetNewDestination()
     {
-        
-        wayPoint = new Vector2(Random.Range(-maxDistance, maxDistance),Random.Range(-maxDistance, maxDistance));
-        
+        float distance = Random.Range(minDistance, maxDistance);
+        float directionRadAngle = Mathf.Deg2Rad * Random.Range(0f, 360f);
+        wayPoint = transform.position + new Vector3(Mathf.Cos(directionRadAngle), Mathf.Sin(directionRadAngle)) * distance;
+        currentSpeed = Random.Range(minSpeed, maxSpeed);
+        Debug.DrawLine(transform.position, new Vector3(wayPoint.x, wayPoint.y, transform.position.z), Color.red, 5f);
     }
 
     private void Move()
     {
-        transform.position = Vector2.MoveTowards(transform.position, wayPoint, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, wayPoint, currentSpeed * Time.deltaTime);
+    }
+
+    private void Wait()
+    {
+        // change anim
+        // sit if long time
     }
 
 
+
+    public enum State
+    {
+        MOVING,
+        WAITING
+    }
 }
 
 
